03dde390bc842e9d2cf755d18d46a531f572c87f
Devin <devdhera@gmail.com>
2019-09-07 21:42:31 +0530

    Added session helper

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,10 +2,9 @@
     // Load Config
     require_once 'config/config.php';
 
-    // Load Libraries
-    // require_once 'libraries/Core.php';
-    // require_once 'libraries/Controller.php';
-    // require_once 'libraries/Database.php';
+    // Load Helpers
+    require_once 'helpers/url_helper.php';
+    require_once 'helpers/session_helper.php';
 
     // Autoload Core Libraries
     spl_autoload_register(function($className)
