6889b333ddd20beefd7143c2f8875ddd665ebadc
Devin <devdhera@gmail.com>
2019-09-09 01:58:02 +0530

    Reconfigured navbar

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -16,12 +16,34 @@
         <li class="nav-item">
           <a class="nav-link" href="<?php echo URLROOT; ?>/pages/about">About</a>
         </li>
+        <?php if(isset($_SESSION['user_id'])) : ?>
+          <li class="nav-item">
+            <a class="nav-link" href="<?php echo URLROOT; ?>/quizzes">Quizzes</a>
+          </li>
+        <?php endif; ?>
       </ul>
 
       <ul class="navbar-nav ml-auto">
-          <?php if(isset($_SESSION['user_id'])) : ?>
-            <li class="nav-item">
-              <a class="nav-link" href="<?php echo URLROOT; ?>/users/logout">Logout</a>
+          <?php if(isset($_SESSION['user_id'])) : ?>            
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <?php echo $_SESSION['user_name']; ?>
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item"><i class="fa fa-user-circle"></i> <?php echo $_SESSION['user_name']; ?></a>
+                <a class="dropdown-item"><i class="fa fa-at"></i> <?php echo $_SESSION['user_email']; ?></a>
+                <a class="dropdown-item"><i class="fa fa-registered"></i> <?php echo $_SESSION['user_role']; ?></a> 
+                <?php if(isAdmin()) : ?>
+                  <div class="dropdown-divider"></div>                
+                  <a class="dropdown-item" href="<?php echo URLROOT; ?>/admins/users">Users</a> 
+                  <a class="dropdown-item" href="<?php echo URLROOT; ?>/admins/attempts">Attempts</a> 
+                  <a class="dropdown-item" href="<?php echo URLROOT; ?>/quizzes/addbasic">Add Quiz</a> 
+                <?php endif; ?>
+                <div class="dropdown-divider"></div>  
+                <a class="dropdown-item" href="<?php echo URLROOT; ?>/users/results">Your Results</a>
+                <a class="dropdown-item" href="<?php echo URLROOT; ?>/users/logout">Logout</a>
+              </div>
+                
             </li>
           <?php else : ?>
             <li class="nav-item">
